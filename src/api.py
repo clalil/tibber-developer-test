3dc36b9bfbdc0f059441b51b694e7eb8735a6f7b
clalil <cliljander@gmail.com>
2024-04-27 18:51:18 +0200

    feat(tibber-developer-test): WIP api response body

--- src/api.py
+++ src/api.py
@@ -1,4 +1,6 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import datetime as datetime
+import time
 # import pdb
 # pdb.set_trace()
 app = Flask(__name__)
@@ -11,38 +13,11 @@ class EndpointClient:
         self.directions = {"north": 1, "east": 1, "south": -1, "west": -1}
         self.duration = 0
         self.sum_moves = len(moves)
-        self.timestamp = ""
         self.unique_moves = 0
 
-    # @staticmethod
-    # def timestamp():
-    #     return datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')
-
-    # def get_coors(self, coordinates):
-    #     x1 = coordinates[0][0][1]
-    #     y1 = coordinates[0][1][1]
-    #     x2 = coordinates[-1][0][1]
-    #     y2 = coordinates[-1][1][1]
-
-    #     return x1, y1, x2, y2
-    
-    # def count_moves(self):
-    #     coordinates = [(("x", self.x), ("y", self.y))]
-    #     increment_x = self.x
-    #     increment_y = self.y
-    #     for move in self.moves:
-    #         if move["direction"] == "east" or move["direction"] == "west":
-    #             increment_x += self.directions[move["direction"]] * move["steps"]
-    #             coordinates.extend([(("x", increment_x), ("y", increment_y))])
-    #         if move["direction"] == "north" or move["direction"] == "south":
-    #             increment_y += self.directions[move["direction"]] * move["steps"]
-    #             coordinates.extend([(("x", increment_x), ("y", increment_y))])
-
-        # x1, y1, x2, y2 = self.get_coors(coordinates)
-        # path_travelled_between_cors = max(abs(x1 - x2),abs(y1 - y2))
-            # calculate if move is unique
-
-# moves = [{'direction': 'east', 'steps': 2}, {'direction': 'north', 'steps': 1}]
+    @staticmethod
+    def timestamp():
+        return datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')
 
     def count_moves(self):
         coordinates = [(("x", self.x), ("y", self.y))]
@@ -50,16 +25,28 @@ class EndpointClient:
         y = self.y
         for move in self.moves:
             for step in range(move["steps"]):
-                print(step)
                 if move["direction"] == "east" or move["direction"] == "west":
                     x += 1
                     coordinates.append((("x", x), ("y", y)))
                 if move["direction"] == "north" or move["direction"] == "south":
                     y += 1
                     coordinates.append((("x", x), ("y", y)))
-        print(coordinates)
-        # breakpoint()
-        return coordinates
+
+        unique_moves = len(set(coordinates))
+
+        return unique_moves
+
+
+    def create_response_body(self):
+        response_body = [{
+            "result": {
+                "timestamp": str(self.timestamp()),
+                "commands": self.sum_moves,
+                "result": self.count_moves(),
+                "duration": 0.1
+            }
+        }]
+        return response_body
 
 
 # score = sum(base_rules[d].get(dice.count(d), 0) for d in set(dice))
@@ -77,16 +64,15 @@ def app_response_body(resp):
     y = resp["start"]["y"]
     moves = resp['commmands']
     client = EndpointClient(x, y, moves)
-    response = client.count_moves()
-    # print(f"Here is response: {client.count_moves()}")
-    return resp
+    resp_body = client.create_response_body()
+    return resp_body
 
 @app.post("/tibber-developer-test/enter-path")
 def create_request():
         if request.is_json:
             request_body = request.get_json(silent=False)
             response_body = app_response_body(request_body)
-            return response_body, 201
+            return jsonify(response_body), 201
         return {"error": "Request must be JSON"}, 415
 
 if __name__ == "__main__":
--- test/test_api.py
+++ test/test_api.py
@@ -27,16 +27,16 @@ def request_body():
 
 @pytest.fixture
 def response_body():
-    response_body = {
-    "robot": {
+    response_body = [{
+    "result": {
         "id": 1,
         "timestamp": "2018-05-12 12:45:10.851596",
         "commands": 2,
         "result": 4,
         "duration": 0.000123
-    }
-    }
-    return response_body
+        }
+    }]
+    return json.dumps(response_body)
 
 # def test__when_making_a_post__should_return_valid_():
 #     url = '/tibber-developer-test/enter-path'
@@ -65,10 +65,10 @@ def response_body():
 #         response = c.post(url, json=request_body)
 #     assert json.loads(response.data) == response_body
 
-
-def test__client_should_return_summary_when_called(request_body):
+def test__client_should_return_summary_when_called(request_body, response_body):
     x = request_body["start"]["x"]
     y = request_body["start"]["y"]
     moves = request_body['commmands']
     client = EndpointClient(x, y, moves)
-    assert response_body == client.count_moves()
+    resp = client.create_response_body()
+    assert response_body == resp
