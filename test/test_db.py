fd7aebd524fc44c53955bb74b480b33ff077cb2e
clalil <cliljander@gmail.com>
2024-04-28 09:26:22 +0200

    refactor(tibber-developer-test): Refactor db tests

--- test/test_db.py
+++ test/test_db.py
@@ -7,34 +7,33 @@ SQL = '''CREATE TABLE IF NOT EXISTS robot
         commands INT, 
         result INT, 
         duration REAL)'''
-        
+
+
 SAMPLE_DATA =[('2018-05-12 12:45:10.851596', 2, 4, 0.000123), ('2018-05-10 12:45:10.851596', 3, 2, 0.000123)]
 
+
 @pytest.fixture
-def create_connection():
+def db():
     conn = sqlite3.connect("test_db")
     with conn:
-        cursor = conn.cursor()
-        cursor.execute(SQL)
+        conn.execute(SQL)
         yield conn
         conn.rollback()
 
 
-def test__when_initialized__should_be_empty(create_connection):
-    cursor = create_connection.cursor()
-    assert len(list(cursor.execute('SELECT * FROM robot'))) == 0
+def test__when_initialized__should_be_empty(db):
+    assert len(list(db.execute('SELECT * FROM robot'))) == 0
 
 
-def test__when_called_upon__should_store_data(create_connection):
-    cursor = create_connection.cursor()
-    cursor.executemany('INSERT INTO robot VALUES(?,?,?,?)', SAMPLE_DATA)
-    assert len(list(cursor.execute('SELECT * FROM robot'))) == 2
+def test__when_called_upon__should_store_data(db):
+    db.executemany('INSERT INTO robot VALUES(?,?,?,?)', SAMPLE_DATA)
+    assert len(list(db.execute('SELECT * FROM robot'))) == 2
 
 
-def test__when_values_are_inserted__should_successfully_populate(create_connection):
-    cursor = create_connection.cursor()
-    cursor.execute('INSERT INTO robot VALUES(?,?,?,?)', ('2018-05-12 12:45:10.851596', 2, 4, 0.000123))
-    cursor.execute('SELECT * FROM robot')
-    rows = cursor.fetchall()
+def test__when_values_are_inserted__should_successfully_populate(db):
+    db.execute('INSERT INTO robot VALUES(?,?,?,?)', ('2018-05-12 12:45:10.851596', 2, 4, 0.000123))
+    db.execute('SELECT * FROM robot')
+    cursor_for_traversing = db.cursor()
+    rows = cursor_for_traversing.fetchall()
     for row in rows:
         assert row == ('2018-05-12 12:45:10.851596', 2, 4, 0.000123)
